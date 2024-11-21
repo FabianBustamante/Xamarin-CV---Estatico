d1e6f7a7ad782c432616195b815f1eb4822f2bef
FabianBustamante <63063558+FabianBustamante@users.noreply.github.com>
2024-11-20 21:58:10 -0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace AppEstatico
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
